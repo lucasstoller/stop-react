56986686dc635cdcd6119b592ff242db930de8cc
Lucas Stoller <lucasstoller@hotmail.com>
2019-05-25 15:38:25 -0300

    Adicionando showcase switch

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Options from './Options';
 
 import bgImage from '../assets/home-bg.png';
+import CreateARoom from '../pages/Home/CreateARoom';
 
 export const Container = styled.div`
   height: 92vh;
@@ -16,13 +17,41 @@ export const Content = styled.div`
   height: 80%;
 `;
 
-function Showcase(){
-  return (
-    <Container>
-      <Content></Content>
-      <Options></Options>
-    </Container>
-  );
+class Showcase extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.handleChangeOption = this.handleChangeOption.bind(this);
+    this.state = { option: 'CreateARoom' }
+  }
+
+  handleChangeOption(option){
+    this.setState({ option });
+  }
+
+  render() {
+    const option = this.state.option;
+    let content;
+
+    switch(option){
+      case 'CreateARoom':
+        content = (
+          <Content>
+            <CreateARoom></CreateARoom>
+          </Content>
+        ) 
+        break;
+      default:
+        content = <Content />
+    }
+
+    return (
+      <Container>
+        { content }
+        <Options option={this.state.option} onChangeOption={this.handleChangeOption}></Options>
+      </Container>
+    );
+  }
 }
 
 export default Showcase;
